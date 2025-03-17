d968c1fac5ad8ace032e03409940cc115247a7c6
flapicat <08marcinb@gmail.com>
2025-03-17 20:55:06 +0100

    Fixing build system

--- generate.bat
+++ generate.bat
@@ -1,2 +1,6 @@
 call vendor\premake\premake5.exe vs2022
+
+xcopy Flapi_Engine\assets bin\Debug-windows-x86_64\Flapi_Engine\assets /E /I /Y
+xcopy Flapi_Engine\assets bin\Release-windows-x86_64\Flapi_Engine\assets /E /I /Y
+
 PAUSE
